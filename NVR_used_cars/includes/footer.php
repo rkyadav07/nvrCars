f30204293c60e8ab4dcc6420f77ecd0308228554
Hemanth Kumar Masarupadi <HemanthKumar_Masarapdi@epam.com>
2024-05-17 17:11:12 +0530

    nvr used cards data added

---
+++ NVR_used_cars/includes/footer.php
@@ -0,0 +1,11 @@
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="inner-content">
+              <p>Copyright © <?php echo date('Y');?> NVR used cars </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
